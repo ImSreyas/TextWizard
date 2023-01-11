1939735b031c7c3172fcb7aec9ad2f74c8e60ba8
sreyasProgrammer <sreyassatheesh755@gmail.com>
2023-01-12 02:10:33 +0530

    login page half completed

---
+++ static/js/inputBorderAnimation.js
@@ -0,0 +1,9 @@
+// let inputs = document.querySelectorAll(".input")
+// inputs.forEach(input => {
+//     input.addEventListener("click", () => {
+//         input.classList.add("move")
+//     })
+//     let newInput = document.createElement("div")
+//     newInput.classList.add('input-border-animation')
+//     input.parentElement.appendChild(newInput)
+// })
