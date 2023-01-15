65477700c2fa45c56458897efb4234a62663802a
sreyasProgrammer <sreyassatheesh755@gmail.com>
2023-01-15 23:26:22 +0530

    working on reg form page

--- public.py
+++ public.py
@@ -2,6 +2,7 @@ from flask import *
 from flask_mysqldb import MySQL
 import database
 import json
+import re
 
 public = Blueprint('public', __name__)
 
@@ -41,15 +42,62 @@ def testPage():
 
 @public.route('/registration', methods = ['POST', 'GET'])
 def registration():
-    if request.method == 'POST' : 
-        name = request.form['name']
-        address = request.form['address']
-        email = request.form['email']
-        phone = request.form['phone']
-        username = email
-        password = request.form['password']
-        query = "INSERT INTO user SET name='"+name+"', address='"+address+"', email='"+ email +"', phone='"+ phone +"'"
+    if request.method == 'POST' :
+        name = request.form.get('name')
+        gender = request.form.get('gender')
+        address = request.form.get('address')
+        email = request.form.get('email')
+        phone = request.form.get('phone')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirmPassword')
+
+        x = ['', '', '', '', '', '', '']
+        
+        if name == None or name == "" :
+            name = ""
+            x[0] = 'please enter a name'  
+        if address == None or address == "" :
+            address = ""
+            x[1] = 'please enter an address'
+        if phone == None or phone == "" :
+            phone = ""
+            x[2] = 'please enter a phone number'
+        if email == None or email == "" :
+            email = ""
+            x[3] = 'please enter an email'
+        if username == None or username == "" :
+            username = ""
+            x[4] = 'please enter a username'
+        if password == None or password == "" :
+            password = ""
+            x[5] = 'please enter a password'
+        if confirm_password == None or confirm_password == "" :
+            confirm_password = ""
+        
+        if not re.fullmatch(r'^[0-9]{10}$', phone) :
+            x[2] = 'invalid phone number...!'
+        if not re.fullmatch(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b', email) :
+            x[3]  = 'invalid email id...!'
+        result = database.select("SELECT * FROM login WHERE username='%s'" % (username))
+        if len(result) > 0 :
+            x[4] = 'user already exist...!'
+        if not re.fullmatch(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$', password) :
+            x[5] = 'read the note below'
+        else : 
+            if password == confirm_password :
+                x[6] = ''
+            else :
+                x[6] = 'enter the same password' 
+        for val in x :
+           if val == '' :
+               continue
+           else :
+               return x
+           
+        query = "INSERT INTO user SET name=%s, gender=%s, address=%s, email=%s, phone=%s" % (name, gender, address, email, phone)
         result = database.insert(query)
+        return 'success'
     return render_template('registration.html')
 
 @public.route('/test')
--- static/js/registrationForm.js
+++ static/js/registrationForm.js
@@ -1,10 +1,46 @@
 let optionContainer = document.querySelectorAll('.gender-container button')
 Array.prototype.forEach.call(optionContainer, option => {
-    console.log('hello')
     option.addEventListener('click', (e) => {
         Array.prototype.forEach.call(optionContainer, option => {
             option.setAttribute('selected', false)
         })
         e.target.setAttribute('selected', true)
+    })
+})
+$(document).ready(()=>{
+    $('.reg-submit').click(()=>{
+        let name = $('.name').val()
+        let address = $('.address').val()
+        let email = $('.email').val()
+        let phone = $('.phone').val()
+        let username = $('.username').val()
+        let password = $('.password').val()
+        let confirmPassword = $('.confirm-password').val()
+        let gender = $('.gender-container button[selected=true]').text()
+        $.ajax({
+            url: '/registration',
+            type: "POST",
+            data : {
+                name: name,
+                gender: gender,
+                address: address,
+                phone: phone,
+                email: email,
+                username: username,
+                password: password,
+                confirmPassword: confirmPassword
+            },
+            success: (data)=>{
+                if(data == 'success'){
+                   location.href = "/login?from='account_created'"
+                }
+                else{
+                    let errorContainer = $('error')
+                    $.each(errorContainer, (index, error) => {
+                        $(error).html(data[index])
+                    })
+                }
+            }
+        })
     })
 })
