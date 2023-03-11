95d4f9cd3684a800210b6dfe379a6f8e8be17f74
ImSreyas <sreyassatheesh755@gmail.com>
2023-03-11 23:18:30 +0530

    the js running twice when the input change occures has been solved in the ocr.js file

--- static/js/ocr.js
+++ static/js/ocr.js
@@ -1,32 +1,41 @@
 let container = document.getElementById('navigationBar');
+let fileChangeListenerAdded = false; // Flag variable
+
 container.addEventListener('click', () => {
   const input = document.querySelector('.image-input');
   input.click();
-  input.addEventListener('change', () => {
-    const file = input.files[0];
-    const url = URL.createObjectURL(file);
-    const img = document.createElement('img');
-    img.src = url;
-    let imageItems = document.querySelectorAll('.drag-and-drop-icon-container img');
-    imageItems.forEach((item) => (item.remove()));
-    document.querySelector('.drag-and-drop-icon-container').append(img);
 
-    // Create a new FormData object
-    const formData = new FormData();
+  if (!fileChangeListenerAdded) { // Check if the event listener has already been added
+    const handleFileChange = () => {
+      const file = input.files[0];
+      const url = URL.createObjectURL(file);
+      const img = document.createElement('img');
+      img.src = url;
+      let imageItems = document.querySelectorAll('.drag-and-drop-icon-container img');
+      imageItems.forEach((item) => (item.remove()));
+      document.querySelector('.drag-and-drop-icon-container').append(img);
 
-    // Add the image file to the form data
-    formData.append('file', file);
+      // Create a new FormData object
+      const formData = new FormData();
 
-    // Send the form data via Ajax
-    $.ajax({
-      url: '/ocrCore',
-      type: "POST",
-      data: formData,
-      processData: false,
-      contentType: false,
-      success: (data) => {
-        $('.text-content').append(data);
-      }
-    });
-  });
+      // Add the image file to the form data
+      formData.append('file', file);
+
+      // Send the form data via Ajax
+      $.ajax({
+        url: '/ocrCore',
+        type: "POST",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: (data) => {
+          $('.text-content').append(data);
+        }
+      });
+    };
+
+    input.addEventListener('change', handleFileChange);
+    fileChangeListenerAdded = true; // Set the flag variable to true
+  }
 });
+
