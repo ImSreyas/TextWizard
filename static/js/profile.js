145529e6b90420e629c8ae5620e49f7d74530cd0
ImSreyas <sreyassatheesh755@gmail.com>
2023-03-22 23:03:46 +0530

    history in profile page is done

--- static/js/profile.js
+++ static/js/profile.js
@@ -60,8 +60,7 @@ $(document).ready(()=>{
         let confirmPassword = $('.confirm-password').val()
         let gender = $('.gender-container button[selected=true]').text()
         let userType = $('.user-type-container button[selected=true]').text()
-        console.log(userType)
-        $.ajax({
+         $.ajax({
             url: '/updateProfile',
             type: "POST",
             data : {
@@ -91,51 +90,60 @@ $(document).ready(()=>{
 // -ocr js
 //? file format selection  
 
-const formatSelectorBtn = document.querySelector('.file-format-container')
-const optionContainer = document.querySelector('.file-format-option-container')
-formatSelectorBtn.addEventListener('click', ()=>{
-  if(optionContainer.getAttribute('vis') == 'false') optionContainer.setAttribute('vis', 'true')
-  else optionContainer.setAttribute('vis', 'false')
-})
+const formatSelectorBtns = document.querySelectorAll('.file-format-container')
+const optionContainers = document.querySelectorAll('.file-format-option-container')
 
-const options = document.querySelectorAll('.file-format-option-container option')
-options.forEach(option => {
-  option.addEventListener('click', (e) => {
-    optionContainer.setAttribute('vis', 'false')
-    formatSelectorBtn.children[0].innerText = e.target.innerText
+formatSelectorBtns.forEach((formatSelectorBtn, index) => {
+  const optionContainer = optionContainers[index]
+  formatSelectorBtn.addEventListener('click', ()=>{
+    if(optionContainer.getAttribute('vis') == 'false') optionContainer.setAttribute('vis', 'true')
+    else optionContainer.setAttribute('vis', 'false')
+  })
+
+  const options = optionContainer.querySelectorAll('option')
+  options.forEach(option => {
+    option.addEventListener('click', (e) => {
+      optionContainer.setAttribute('vis', 'false')
+      formatSelectorBtn.children[0].innerText = e.target.innerText
+    })
   })
 })
+
 // ?end of file format selection 
 // ? download btn 
-const downloadBtn = document.querySelector('.txt-download-link')
-downloadBtn.addEventListener('click', () => {
-  const textFormat = document.querySelector('.file-format-container .actualFileFormat').innerText
-  const text = getTextFromEditor()
-  const fileName = document.querySelector('.file-name-input').value
-  switch(textFormat){
-    case ".txt": txtDownload(text, fileName)
-    break
-    case ".html": htmlDownload(text, fileName)
-    break
-    case ".css": cssDownload(text, fileName)
-    break
-    case ".csv": csvDownload(text, fileName)
-    break
-    case ".json": jsonDownload(text, fileName)
-    break
-    case ".xml": xmlDownload(text, fileName)
-    break
-    case ".md": mdDownload(text, fileName)
-    break
-    case ".pdf": pdfDownload(text, fileName)
-    break
-    default : txtDownload(text, fileName)
-  }
+const downloadBtns = document.querySelectorAll('.txt-download-link')
+
+downloadBtns.forEach((downloadBtn, index) => {
+  downloadBtn.addEventListener('click', () => {
+    const textFormat = document.querySelectorAll('.file-format-container')[index].querySelector('.actualFileFormat').innerText
+    const text = getTextFromEditor(index)
+    const fileName = document.querySelectorAll('.file-name-container')[index].querySelector('.file-name-input').value
+
+    switch(textFormat){
+      case ".txt": txtDownload(text, fileName, index)
+      break
+      case ".html": htmlDownload(text, fileName, index)
+      break
+      case ".css": cssDownload(text, fileName, index)
+      break
+      case ".csv": csvDownload(text, fileName, index)
+      break
+      case ".json": jsonDownload(text, fileName, index)
+      break
+      case ".xml": xmlDownload(text, fileName, index)
+      break
+      case ".md": mdDownload(text, fileName, index)
+      break
+      case ".pdf": pdfDownload(text, fileName, index)
+      break
+      default : txtDownload(text, fileName, index)
+    }
+  })
 })
 // ? end of download btn
 //? function for getting the text from editor 
-const getTextFromEditor = () => {
-  let paragraphs = document.querySelectorAll('.text-content p');
+const getTextFromEditor = (i) => {
+  let paragraphs = document.querySelectorAll(".text-content")[i].querySelectorAll('p');
   let data = "";
   paragraphs.forEach((p, index) => {
     data += p.innerText.trim();
@@ -146,58 +154,58 @@ const getTextFromEditor = () => {
   return data;
 }
 //? txt download function
-function txtDownload(data, fileName){
-  const downloadLink = document.querySelector('.txt-download-link');
-  downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'text/plain' }));
+function txtDownload(data, fileName, index){
+  const downloadLink = document.querySelectorAll('.txt-download-link')[index]
+    downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'text/plain' }))
+    if(fileName != ''){
+        downloadLink.download = fileName + '.txt'
+    }
+}
+function htmlDownload(data, fileName, index){
+  const downloadLink = document.querySelectorAll('.txt-download-link')[index]
+  downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'text/html' }))
   if(fileName != ''){
     downloadLink.download = fileName + '.txt'
   }
 }
-function htmlDownload(data, fileName){
-  const downloadLink = document.querySelector('.txt-download-link');
-  downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'text/html' }));
+function cssDownload(data, fileName, index){
+  const downloadLink = document.querySelectorAll('.txt-download-link')[index]
+  downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'text/css' }))
   if(fileName != ''){
     downloadLink.download = fileName + '.txt'
   }
 }
-function cssDownload(data, fileName){
-  const downloadLink = document.querySelector('.txt-download-link');
-  downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'text/css' }));
+function jsonDownload(data, fileName, index){
+  const downloadLink = document.querySelectorAll('.txt-download-link')[index]
+  downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'application/json' }))
   if(fileName != ''){
     downloadLink.download = fileName + '.txt'
   }
 }
-function jsonDownload(data, fileName){
-  const downloadLink = document.querySelector('.txt-download-link');
-  downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'application/json' }));
-  if(fileName != ''){
-    downloadLink.download = fileName + '.txt'
-  }
-}
-function xmlDownload(data, fileName){
-  const downloadLink = document.querySelector('.txt-download-link');
-  downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'application/xml' }));
+function xmlDownload(data, fileName, index){
+  const downloadLink = document.querySelectorAll('.txt-download-link')[index]
+  downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'application/xml' }))
   if(fileName != ''){
     downloadLink.download = fileName + '.xml'
   }
 }
-function csvDownload(data, fileName){
-  const downloadLink = document.querySelector('.txt-download-link');
-  downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'text/csv' }));
+function csvDownload(data, fileName, index){
+  const downloadLink = document.querySelectorAll('.txt-download-link')[index]
+  downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'text/csv' }))
   if(fileName != ''){
     downloadLink.download = fileName + '.csv'
   }
 }
-function mdDownload(data, fileName){
-  const downloadLink = document.querySelector('.txt-download-link');
-  downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'text/markdown' }));
+function mdDownload(data, fileName, index){
+  const downloadLink = document.querySelectorAll('.txt-download-link')[index]
+  downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'text/markdown' }))
   downloadLink.download = 'markdownFile.md'
   if(fileName != ''){
     downloadLink.download = fileName + '.md'
   }
 }
-function pdfDownload(data, fileName){
-  const downloadLink = document.querySelector('.txt-download-link');
+function pdfDownload(data, fileName, index){
+  const downloadLink = document.querySelectorAll('.txt-download-link')[index]
   downloadLink.href = URL.createObjectURL(new Blob([data], { type: 'application/pdf' }));
   if(fileName != ''){
     downloadLink.download = fileName + '.pdf'
@@ -224,37 +232,60 @@ function pdfDownload(data, fileName){
 
 //-toolbox
 //?copy
-const copyBtn = document.querySelector('.tool-box .copy')
-copyBtn.addEventListener('click', () => {
-    navigator.clipboard.writeText(getTextFromEditor())
+const copyBtn = document.querySelectorAll('.tool-box .copy')
+copyBtn.forEach((cp, index) => {
+    cp.addEventListener('click', () => {
+        navigator.clipboard.writeText(getTextFromEditor(index))
+    })
 })
+
 //?share
-const shareBtn = document.querySelector('.tool-box .share')
-if(shareBtn){
-    shareBtn.addEventListener('click', () => {
-        const textFormat = document.querySelector('.file-format-container-new .actualFileFormatNew').innerText
-        const shareList = document.querySelector('.share-list-container')
-        if(shareList.getAttribute('vis') == 'false') shareList.setAttribute('vis', 'true')
-        else shareList.setAttribute('vis', 'false')
-    })
-}
+const shareBtn = document.querySelectorAll('.tool-box .share')
+shareBtn.forEach((sb, index) => {
+    if(sb){
+        sb.addEventListener('click', () => {
+            const textFormat = document.querySelectorAll('.file-format-container-new .actualFileFormatNew')[index].innerText
+            const shareList = document.querySelectorAll('.share-list-container')[index]
+            if(shareList.getAttribute('vis') == 'false'){
+                shareList.setAttribute('vis', 'true')
+                const users = document.querySelectorAll(".user-data-container[selected='true']")
+                users.forEach(user => {
+                    user.setAttribute('selected', 'false')
+                })
+                const userList = document.querySelectorAll('.user-data-container')
+                userList.forEach(user => {
+                    user.style.display = 'grid'
+                })
+            } 
+            else shareList.setAttribute('vis', 'false')
+        })
+    }
+})
 
-const formatSelectorBtnNew = document.querySelector('.file-format-container-new')
-const optionContainerNew = document.querySelector('.file-format-option-container-new')
-if(formatSelectorBtnNew){
-    formatSelectorBtnNew.addEventListener('click', ()=>{
-        if(optionContainerNew.getAttribute('vis') == 'false') optionContainerNew.setAttribute('vis', 'true')
-        else optionContainerNew.setAttribute('vis', 'false')
-    })
-}
+
+const formatSelectorBtnNews = document.querySelectorAll('.file-format-container-new')
+const optionContainerNews = document.querySelectorAll('.file-format-option-container-new')
+formatSelectorBtnNews.forEach((formatSelectorBtnNew, index) => {
+    if(formatSelectorBtnNew){
+        formatSelectorBtnNews[index].addEventListener('click', ()=>{
+            if(optionContainerNews[index].getAttribute('vis') == 'false') optionContainerNews[index].setAttribute('vis', 'true')
+            else optionContainerNews[index].setAttribute('vis', 'false')
+        })
+    }
+})
 
 
-const optionsNew = document.querySelectorAll('.file-format-option-container-new option')
-if(optionsNew){
-    optionsNew.forEach(option => {
-        option.addEventListener('click', (e) => {
-            optionContainerNew.setAttribute('vis', 'false')
-            formatSelectorBtnNew.children[0].innerText = e.target.innerText
+
+const optionsNews = document.querySelectorAll('.file-format-option-container-new')
+let options
+if(optionsNews){
+    optionsNews.forEach((option, i) => {
+        options = option.querySelectorAll('option')
+        options.forEach((o) => {
+            o.addEventListener('click', (e) => {
+                optionContainerNews[i].setAttribute('vis', 'false')
+                formatSelectorBtnNews[i].children[0].innerText = e.target.innerText
+            })
         })
     })
 }
@@ -268,8 +299,9 @@ users.forEach(user => {
     })
 })
 // -search 
-const userSearch = document.querySelector('.user-search')
-if(userSearch){
+const userSearch_ = document.querySelectorAll('.user-search')
+userSearch_.forEach(userSearch => {
+    if(userSearch){
     userSearch.addEventListener('keyup', (e)=>{
         let searchValue = e.target.value
         searchValue = searchValue.toLowerCase()
@@ -284,36 +316,43 @@ if(userSearch){
         })
     })
 }
+})
+
 // -final share btn 
-const finalShare = document.querySelector('.share-btn-final')
-if(finalShare){
-    finalShare.addEventListener('click', () => {
-        const selectedUsers = document.querySelectorAll(".user-data-container[selected='true']")
-        let selectedUserIds = []
-        selectedUsers.forEach(user => {
-            selectedUserIds.push(parseInt(user.id))
+const finalShare_ = document.querySelectorAll('.share-btn-final')
+finalShare_.forEach((finalShare, index) => {
+    if(finalShare){
+        finalShare.addEventListener('click', () => {
+            const selectedUsers = document.querySelectorAll(".user-data-container[selected='true']")
+            let selectedUserIds = []
+            selectedUsers.forEach(user => {
+                selectedUserIds.push(parseInt(user.id))
+            })
+            const fileName = document.querySelectorAll('.file-name-input')[index].value
+            const fileFormat = document.querySelectorAll('.file-format-container-new')[index].querySelector('.actualFileFormatNew').innerHTML
+            const text = getTextFromEditor(index)
+            $.ajax({
+                url: '/share',
+                type: "POST",
+                data: {
+                    userList: JSON.stringify(selectedUserIds),
+                    text: text,
+                    fileName: fileName,
+                    fileFormat: fileFormat
+                },
+                success: () => {
+                    const users = document.querySelectorAll('.user-data-container')
+                    users.forEach(user => {
+                        user.setAttribute('selected', 'false')
+                    })
+                    document.querySelectorAll('.share-list-container').forEach(list => {
+                        list.setAttribute('vis', 'false')
+                    })
+                    popup('shared successfully', 'green', '4s')
+                }
+            })
         })
-        const fileName = document.querySelector('.file-name-input').value
-        const fileFormat = document.querySelector('.file-format-container-new .actualFileFormatNew').innerHTML
-        const text = getTextFromEditor()
-        $.ajax({
-            url: '/share',
-            type: "POST",
-            data: {
-                userList: JSON.stringify(selectedUserIds),
-                text: text,
-                fileName: fileName,
-                fileFormat: fileFormat
-            },
-            success: () => {
-                const users = document.querySelectorAll('.user-data-container')
-                users.forEach(user => {
-                    user.setAttribute('selected', 'false')
-                })
-                document.querySelector('.share-list-container').setAttribute('vis', 'false')
-                popup('shared successfully', 'green', '4s')
-            }
-        })
-    })
-}
+    }
+})
+
 //  -ocr js end 
